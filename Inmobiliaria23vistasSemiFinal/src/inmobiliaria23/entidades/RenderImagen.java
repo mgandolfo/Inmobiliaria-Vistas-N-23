029ab6aa57e68b063928cfb1c1a1f7d2200dcdf8
mgandolfo <mgandolfo2017@gmail.com>
2023-10-20 06:26:09 -0300

    Renderizado

---
+++ Inmobiliaria23vistasSemiFinal/src/inmobiliaria23/entidades/RenderImagen.java
@@ -0,0 +1,22 @@
+package inmobiliaria23.entidades;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class RenderImagen extends DefaultTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        if (value instanceof JLabel) {
+            JLabel lbl = (JLabel) value;
+            return lbl;
+
+        }
+        return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+    }
+
+}
